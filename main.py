48f3d850c6dbb04b3eb45f44a5dc734d2995d3b8
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-09-08 11:11:44 +0100

    CRUD operation with file storage

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 from models.posts import Post
@@ -25,13 +25,45 @@ async def get_posts():
     file_storage.reload()
     file_storage.all(Post)
     return {"data": file_storage.all(Post)}
-@app.post("/posts/")
+@app.post("/posts/", status_code=status.HTTP_201_CREATED)
 async def create_post(post: Post):
     file_storage.new(post)
     file_storage.save()
     return {"id": post.id ,"title": post.title, "content": post.content}
 
+
+@app.get("/posts/latest")
+async def get_latest_post():
+    file_storage.reload()
+    posts = file_storage.all(Post).values()
+    latest = max(posts, key=lambda post: post.id)
+    return {"data": latest}
 @app.get("/posts/{post_id}")
-async def get_post(post_id: int):
+async def get_post(post_id: int, response: Response):
     post = find_post(post_id)
+    if post is None:
+       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
     return {"data": post}
+
+@app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(post_id: int):
+    post = find_post(post_id)
+    if post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    file_storage.delete(post)
+    file_storage.save()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{post_id}")
+async def update_post(post_id: int, post: Post):
+    post = find_post(post_id)
+    if post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    post.title = post.title
+    post.content = post.content
+    post.published = post.published
+    post.rating = post.rating
+    file_storage.save()
+    return {"data": post}
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -49,7 +49,7 @@ class FileStorage:
     def delete(self, obj=None):
         """ Delete object from __objects """
         if obj is not None:
-            key = obj.__class__.__name__ + "." + obj.id
+            key = obj.__class__.__name__ + "." + str(obj.id)
             if key in self.__objects:
                 del self.__objects[key]
 
