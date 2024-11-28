9d37af8c6950b09050bdea56582a67daccb1227f
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-28 16:46:56 +0100

    validate env variables using pydantic

--- app/db_storage.py
+++ app/db_storage.py
@@ -5,7 +5,7 @@ import dotenv
 from .config import settings
 
 dotenv.load_dotenv()
-SQLALCHEMY_DB_URL = f'postgresql://{settings.db_user}:{settings.db_password}@{settings.db_host}/{settings.db_name}'
+SQLALCHEMY_DB_URL = f'postgresql://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}'
 engine = create_engine(SQLALCHEMY_DB_URL)
 session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
